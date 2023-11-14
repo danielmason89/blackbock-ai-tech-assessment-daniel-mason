05db854756e49284a6286541106e1f48feb2a84f
Daniel Mason <danmasson0@gmail.com>
2023-11-14 11:41:13 -0500

    resolve build issues part.3

--- app/api/auth/[...nextauth]/signout.ts
+++ app/api/auth/[...nextauth]/signout.ts
@@ -1,6 +1,18 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { signOut } from 'next-auth/react';
+// import { NextApiRequest, NextApiResponse } from 'next';
+// import { getSession, Session } from 'next-auth/react';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await signOut({ req, res, callbackUrl: '/sign-in' });
-}
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   // Get the current session
+//   const session: Session | null = await getSession({ req });
+
+//   if (session) {
+//     // Handle the session token invalidation here
+//     // This might involve your custom logic to invalidate the session token
+
+//     // Redirect the user after signing out
+//     res.redirect('/sign-in');
+//   } else {
+//     // Handle the case where there is no session (user not signed in)
+//     res.status(401).json({ message: 'No active session' });
+//   }
+// }
